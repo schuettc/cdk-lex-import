ca1358d977a3938dc4add74003fc472ad4e5b586
Court Schuett <schuettc@amazon.com>
2024-11-02 00:06:28 -0500

    fix: update deps

---
+++ .projenrc.ts
@@ -0,0 +1,47 @@
+const { awscdk } = require('projen');
+const { UpgradeDependenciesSchedule } = require('projen/lib/javascript');
+
+const project = new awscdk.AwsCdkConstructLibrary({
+  author: 'Court Schuett',
+  projenrcTs: true,
+  authorAddress: 'https://subaud.io',
+  jest: false,
+  cdkVersion: '2.20.0',
+  defaultReleaseBranch: 'main',
+  name: 'cdk-lex-zip-import',
+  description: 'cdk construct for importing a zipped Lex bot',
+  keywords: ['cdk', 'lex', 'import', 'bot'],
+  repositoryUrl: 'https://github.com/schuettc/cdk-lex-zip-import.git',
+  eslintOptions: {
+    ignorePatterns: ['example/**'],
+  },
+  depsUpgradeOptions: {
+    ignoreProjen: false,
+    workflowOptions: {
+      labels: ['auto-approve', 'auto-merge'],
+      schedule: UpgradeDependenciesSchedule.WEEKLY,
+    },
+  },
+  autoApproveOptions: {
+    secret: 'GITHUB_TOKEN',
+    allowedUsernames: ['schuettc'],
+  },
+  python: {
+    distName: 'cdk-lex-zip-import',
+    module: 'cdk_lex_zip_import',
+  },
+  autoApproveUpgrades: true,
+  projenUpgradeSecret: 'PROJEN_GITHUB_TOKEN',
+});
+const common_exclude = [
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  'dependabot.yml',
+];
+
+project.npmignore.exclude(...common_exclude);
+project.gitignore.exclude(...common_exclude);
+project.gitignore.include('example');
+
+project.synth();
